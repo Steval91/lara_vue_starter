b3bd236a75909178c0d170e273b570230156863b
Steval91 <167100080+Steval91@users.noreply.github.com>
2025-03-18 11:02:18 +0700

    Add Prime Vue and Prime Icon.

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -1,7 +1,13 @@
+import 'primeicons/primeicons.css';
 import '../css/app.css';
 
 import { createInertiaApp } from '@inertiajs/vue3';
+import { definePreset } from '@primeuix/themes';
+import Aura from '@primeuix/themes/aura';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import PrimeVue from 'primevue/config';
+import Ripple from 'primevue/ripple';
+import ToastService from 'primevue/toastservice';
 import type { DefineComponent } from 'vue';
 import { createApp, h } from 'vue';
 import { ZiggyVue } from 'ziggy-js';
@@ -22,6 +28,30 @@ declare module 'vite/client' {
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
+const MyPreset = definePreset(Aura, {
+    semantic: {
+        primary: {
+            50: '{gray.50}',
+            100: '{gray.100}',
+            200: '{gray.200}',
+            300: '{gray.300}',
+            400: '{gray.400}',
+            500: '{gray.500}',
+            600: '{gray.600}',
+            700: '{gray.700}',
+            800: '{gray.800}',
+            900: '{gray.900}',
+            950: '{gray.950}',
+        },
+        focusRing: {
+            width: '1px',
+            style: 'solid',
+            color: '{gray.500}',
+            offset: '2.5px',
+        },
+    },
+});
+
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
     resolve: (name) => resolvePageComponent(`./pages/${name}.vue`, import.meta.glob<DefineComponent>('./pages/**/*.vue')),
@@ -29,6 +59,14 @@ createInertiaApp({
         createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue)
+            .use(PrimeVue, {
+                theme: {
+                    preset: MyPreset,
+                },
+                ripple: true,
+            })
+            .use(ToastService)
+            .directive('ripple', Ripple)
             .mount(el);
     },
     progress: {
